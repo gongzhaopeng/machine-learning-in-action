f14381b4730094373f031c19b43891eda7aab67a
gongzhaopeng <gongguochuanqi@gmail.com>
2020-07-20 11:37:43 +0800

    ...

---
+++ CH2/kNN.py
@@ -0,0 +1,22 @@
+from numpy import *
+import operator
+
+
+def create_dataset():
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify0(in_x, dataset, labels, k):
+    dataset_size = dataset.shape[0]
+    diff_mat = tile(in_x, (dataset_size, 1)) - dataset
+    sq_diff_mat = diff_mat ** 2
+    sq_distances = sq_diff_mat.sum(axis=1)
+    distances = sq_distances ** 0.5
+    sorted_dist_indices = distances.argsort()
+    class_count = {}
+    for i in range(k):
+        vote_i_label = labels[sorted_dist_indices[i]]
+        class_count[vote_i_label] = class_count.get(vote_i_label, 0) + 1
+    sorted_class_count = sorted()  # TODO
